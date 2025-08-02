e24692b6e42f494153b54210beba371b570f0e71
ambar-5 <ambargautam101@gmail.com>
2025-08-02 19:26:41 +0530

    Task Completed

---
+++ Page.js
@@ -0,0 +1,9 @@
+// Navbar scroll effect
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.style.backgroundColor = "#1a252f";
+  } else {
+    navbar.style.backgroundColor = "#2c3e50";
+  }
+});
